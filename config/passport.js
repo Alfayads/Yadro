d4c5245f3b541677a170655fdd944ee174bfd4ce
alfayads <alfayadshameer056@gmail.com>
2024-11-07 22:34:55 +0530

    passport edited

--- config/passport.js
+++ config/passport.js
@@ -7,7 +7,7 @@ var GoogleStrategy = require('passport-google-oauth20').Strategy;
 passport.use(new GoogleStrategy({
     clientID: keys.GOOGLE_CLIENT_ID,
     clientSecret: keys.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/google/callback",
+    callbackURL: "https://yadro.site/google/callback",
     passReqToCallback: true
 },
     async function (req, accessToken, refreshToken, profile, done) {
